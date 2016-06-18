6630505aa844ba2d7eb4524ab2b8574842b7250e
maxprzemo <przemekbanasiak1@wp.pl>
2016-06-18 11:15:48 +0200

    fptest

--- device.mk
+++ device.mk
@@ -124,10 +124,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
-PRODUCT_PACKAGES += \
-    fingerprint.msm8916 \
-    fingerprintd
-
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
